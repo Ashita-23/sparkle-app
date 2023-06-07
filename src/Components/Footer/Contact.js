c6dab2bf0d6922f08f526f57a6cc237a61e6f0d6
Akash <akashbytes@gmail.com>
2023-06-07 23:16:08 +0530

    added validation

--- src/Components/Footer/Contact.js
+++ src/Components/Footer/Contact.js
@@ -1,64 +1,125 @@
-import React, { useState } from "react";
+import React, { useState ,  useEffect } from "react";
 import "./Contact.css";
+import "./ContactMedia.css";
+
+
+
 
 const Contact = () => {
-  const [inputData, setNewInput] = useState({
-    name: " ",
-    phoneNo: " ",
-    email: " ",
-    message: " ",
-  });
+  const InitialValues = { usersName: " ", usersPhNo: " ", usersEmail: " ", usersMessage: " ",}
+  const [inputData, setNewInput] = useState(InitialValues);
+  const [FormErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  console.log(FormErrors,'formerrors')
+
+
+
+  const OnDataEnter = (e) =>{
+    // console.log(e.target.value,e.target.name)
+    const {name,value}  = e.target;
+
+    setNewInput({...inputData,[name]:value})
+    console.log(inputData)
+    // console.log("name",name)
+    // console.log("value",value)
+  }
+
+
+const OnSubmitHanlder = (e)=>{ 
+  e.preventDefault()
+  setFormErrors(FormValidation(inputData))
+  setIsSubmit(true)
+ }
+
+
+const FormValidation = (values)=>{
+  const errors = {} ;
+  console.log(errors,'error')
+  const EmailRegex = /^[a-zA-Z0-9]+@+[a-zA-Z0-9]+.+[A-z]/ ;
+  const PhNoRegex = /^\d{10}$/;
+
+  if(values.usersName === " "){
+    errors.usersName = "User name is required!";
+  }
+  if(values.usersPhNo === " "){
+    errors.usersPhNo = "User Contact no is required!";
+  }else if(!PhNoRegex.test(values.usersPhNo)){
+    errors.usersPhNo = "Please enter valid number!";
+  }
+  if(values.usersEmail === " "){
+    errors.usersEmail = "User Email is required!";
+  }else if(!EmailRegex.test(values.usersEmail)){
+    errors.usersEmail = "Please enter valid email!";
+  }
+  return errors
+}
+
+ useEffect(()=>{
+   console.log(FormErrors,'formErrors-box')
+  if(Object.values(FormErrors).length ===0 && isSubmit){
+    console.log(inputData,'fromUseEffect')
+  }},[FormErrors])
+
   return (
     <>
-      <div className="contact-containor">
-        <p className="contact-heading">Contact Us </p>
-        <form className="contact-form">
+      <div className="contact-containor" id="contact" >
+        <h2 className="contact-heading">Contact Us </h2>
+        {/* {(Object.values(FormErrors).length ===0 && isSubmit)? <h3>form iss  sucessfuly send!</h3> : " "} */}
+        <form className="contact-form" onSubmit={OnSubmitHanlder}>
           <label htmlFor="">
             Name
             <input
               type={"text"}
-              placeholder=" Enter Your Name"
-              name={"name"}
-              value={inputData.name}
-              onChange={(e) => setNewInput(e.target.value)}
-              autoComplete={"off"}
+              name="usersName"
+              // placeholder=" Enter Your Name"
+              value={inputData.usersName}
+              onChange={(e) =>OnDataEnter(e)}
+              autoComplete="off"
             ></input>
+          
           </label>
+          <p className="error-text">{FormErrors.usersName}</p>
           <label htmlFor="">
             Phone No.
             <input
               type={"phone"}
-              placeholder=" Enter Your Phone No. "
-              name={"phoneNo"}
-              value={inputData.phoneNo}
-              onChange={(e) => setNewInput(e.target.value)}
-              autoComplete={"off"}
+              name="usersPhNo"
+              // placeholder=" Enter Your Phone No. "
+              value={inputData.usersPhNo}
+              onChange={(e) =>OnDataEnter(e)}
+              autoComplete="off"
             ></input>
+            
+
           </label>
+          <p className="error-text">{FormErrors.usersPhNo}</p>
+
           <label htmlFor="">
             Email Id.
             <input
               type={"email"}
-              placeholder=" Enter Your Email"
-              name={"email"}
-              value={inputData.email}
-              onChange={(e) => setNewInput(e.target.value)}
-              autoComplete={"off"}
+              name="usersEmail"
+              // placeholder=" Enter Your Email"
+              value={inputData.usersEmail}
+              onChange={(e) =>OnDataEnter(e)}
+              autoComplete="off"
             ></input>
           </label>
+          <p className="error-text">{FormErrors.usersEmail}</p>
+
           <label htmlFor="message">
             message...
             <textarea
               type={"text"}
-              name={"message"}
-              placeholder="Enter Your Meassage ...."
-              value={inputData.message}
-              onChange={(e) => setNewInput(e.target.value)}
+              name="usersMessage"
+              // placeholder="Enter Your Meassage ...."
+              value={inputData.usersMessage}
+              onChange={(e) =>OnDataEnter(e)}
               autoComplete="off"
             ></textarea>
           </label>
           <label htmlFor="submit">
-            <button className="form-btn">submit</button>
+            <button className="form-btn" >submit</button>
           </label>
         </form>
       </div>
