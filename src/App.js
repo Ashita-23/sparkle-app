d9613137c00961f34ca076b358fa06d3d2ceda90
Akash <akashbytes@gmail.com>
2023-06-07 23:26:48 +0530

    added components'

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import "./Components/Header/utilityStyle.css";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Header/Home";
-import ServiceBox from "./Components/Body/Service";
-import Menu from "./Components/Body/Menu";
-import About from "./Components/Body/About";
+import ServiceBox from './Components/Body/ServiceComponents/Service';
+import Menu from "./Components/Body/Menu/Menu";
+import About from "./Components/Body/About/About";
 import Contact from "./Components/Footer/Contact";
 import Footer from "./Components/Footer/Footer";
+// import Errorpage from "./Components/Body/Errorpage";
 
 function App() {
   return (
