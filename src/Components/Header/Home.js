3595b675ff1e3537a861306697a6a00adb47a7ae
Akash <akashbytes@gmail.com>
2023-06-07 23:12:26 +0530

    nothing has changed

--- src/Components/Header/Home.js
+++ src/Components/Header/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Home.css";
+import "./HomeMedia.css";
 
 const Home = () => {
   return (
     <>
-    
-      <div className="home-containor">
+      <div className="home-containor" id="home">
         <div className="home-card">
           <h1 className="card-heading">SPARKLE PARTY ORGANISER</h1>
           <h2 className="card-subheading">Party Organisers, Planners in Haryana</h2>
@@ -17,7 +17,7 @@ const Home = () => {
             answer! If you are looking for the best birthday party organizers in
             Haryana, your search ends at GP Events.
           </p>
-          <button className="home-btn">Conatct us </button>
+          <a href="#contact"> <button className="home-btn">Conatct us </button></a>
         </div>
       </div>
     </>
