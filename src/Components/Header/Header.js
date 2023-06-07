6856cc9bf02facf3ddc1fd31527df092cedb3b7c
Akash <akashbytes@gmail.com>
2023-06-07 23:13:49 +0530

    nothing has changed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,48 @@
-import React from "react"
+
+import { useState } from "react";
 import "./Header.css"
+import './HeaderMedia.css'
+// import {Link} from "react-scroll"
 const Header = ()=>{
+
+const [openNav , setOpenNav] = useState(false)
+
+function ToggleHandler(){
+    if(openNav === false){
+        setOpenNav(true)
+    }else if (openNav === true){
+        setOpenNav(false)
+    }
+}
+
     return(<>
- <div className="header-box">
     <nav className="header-nav">
+    {/* logo */}
         <div className="nav-logo"> <i className="fa-solid fa-star-half-stroke"></i><span>{" "}</span>Sparkle</div>
-        <div className="nav-links">
-        <ul>
-         <li><a href="src\Components\Header\Home.js">home</a></li>
-         <li><a href="src\Components\Body\Service.js">services</a></li>
-         <li><a href="src\Components\Footer\Contact.js">contact</a></li>
-         <li><a href="src\Components\Body\About.js">about</a></li>
+    
+            {/* bar-icon */}
+        <div className="link_btn"  >
+            <span onClick={()=>ToggleHandler()} ><i className="fa-solid fa-bars bar_icon" ></i></span>
+        </div> 
+
+        {/* nav-link */}
+       
+        <ul className= "nav-link-box ">
+         <li className="nav-links"><a href="#home" className="nav-links-a " >home</a></li>
+         <li className="nav-links"><a href="#service" className="nav-links-a " >services</a></li>
+         <li className="nav-links"><a href="#contact"  className="nav-links-a ">Contact</a></li>
+         <li className="nav-links"><a href="#about" className="nav-links-a " >About</a></li>
         </ul>
-        </div>
-        <div className="link_btn">
-            <span><i className="fa-solid fa-bars bar_icon"></i></span>
-        </div>
-    </nav>
- </div>
+
+    
+        </nav>
+        <ul className= {!openNav ? "toggle-link-box hide-toggle-list":"toggle-link-box" }>
+         <li className="toggle-links"><a href="#home"  className="toggle-links-a"  >home</a></li>
+         <li  className="toggle-links"><a href="#service" className="toggle-links-a"  >services</a></li>
+         <li  className="toggle-links"><a href="#contact" className="toggle-links-a"  >Contact</a></li>
+         <li  className="toggle-links"><a href="#about" className="toggle-links-a"  >About</a></li>
+        </ul>
+
     </>)
 } ;
 
